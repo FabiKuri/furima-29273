03c487fcc89f72ed09023f93d1e5f7079e1edf35
FabiKuri <fabien.k.main@gmail.com>
2020-09-10 20:35:40 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -25,11 +25,18 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to item_path
     else
       render :edit
     end
+  end
+
+  def destroy
+      @item = Item.find(params[:id])
+      @item.destroy
+      redirect_to root_path
   end
 
   def checked
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :user
   root to: 'items#index'
   resources :users, only: [:create]
-  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :items
   get 'items/:price', to: 'items#checked'
 end
