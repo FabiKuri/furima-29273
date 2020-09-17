d38339ca7ee63a305451fa8bca5cd2affbdbaae7
FabiKuri <fabien.k.main@gmail.com>
2020-09-17 19:06:17 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_09_11_103251) do
     t.integer "shipping_day_id", null: false
     t.integer "price", null: false
     t.integer "user_id", null: false
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -67,17 +68,14 @@ ActiveRecord::Schema.define(version: 2020_09_11_103251) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname"
-    t.string "email"
-    t.string "password"
-    t.string "family_name"
-    t.string "first_name"
-    t.string "family_name_reading"
-    t.string "first_name_reading"
-    t.date "birthday"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_reading", null: false
+    t.string "first_name_reading", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
